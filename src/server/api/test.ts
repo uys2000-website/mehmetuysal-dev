62501fbda52096600715fd0b7b3098a07f6af733
Mehmet Uysal <mehmetuysal2000@yandex.com>
2023-08-03 01:56:22 +0300

    [MD-2]
    [MD-2] Firebase deploy settings added

--- src/server/api/test.ts
+++ src/server/api/test.ts
@@ -1,4 +1,5 @@
 export default defineEventHandler(function () {
+  app;
   return {
     api: "test",
   };
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export default {
-  isDebugWithStringMode: true,
-  isDebugMode: true,
+  isDebugWithStringMode: false,
+  isDebugMode: false,
 };
